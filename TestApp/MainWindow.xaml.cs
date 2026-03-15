510295a179061900a91c1c8eee6ba9278748431c
Oleksandr Overchuk <alek.owerczuk@gmail.com>
2026-03-15 21:02:05 +0100

    Initial WPF project

---
+++ TestApp/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestApp;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void CalcBtn_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ResetBtn_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+}
